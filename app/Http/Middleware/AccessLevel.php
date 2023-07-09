4dd8ec87e0f8b83076f7feb05683500bb5224fe7
1bertojunior <hjunior854@gmail.com>
2023-07-09 13:45:55 -0300

    fixing access level logic, in AccessLevel middleware

--- app/Http/Middleware/AccessLevel.php
+++ app/Http/Middleware/AccessLevel.php
@@ -9,12 +9,12 @@ class AccessLevelMiddleware
     public function handle($request, Closure $next, $requiredLevel)
     {
         $user = $request->user();
-        // echo "requiredLevel: " . $requiredLevel;
-        // echo "\nuserAccessLevel: " . $user->accessLevel->name;
+        echo "requiredLevel: " . $requiredLevel;
+        echo "\nuserAccessLevel: " . $user->accessLevel->id;
 
         $result =  response()->json(['error' => 'Acesso não autorizado.'], 403);
         
-        if ($user && $user->accessLevel->id >= $requiredLevel) {
+        if ($user && $user->accessLevel->id <= $requiredLevel) {
             $result = $next($request);
         }
 
