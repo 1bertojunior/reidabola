b53200b4d92a03ada892d3f09f233e137b3d7b2a
1bertojunior <hjunior854@gmail.com>
2023-07-12 22:41:40 -0300

    Implement MatchCardsStats in the API with route, model, controller, and seed for [feature/endpoint]

--- app/Http/Controllers/MatchCardsStatsController.php
+++ app/Http/Controllers/MatchCardsStatsController.php
@@ -2,85 +2,119 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\MatchCardsStats;
-use App\Http\Requests\StoreMatchCardsStatsRequest;
-use App\Http\Requests\UpdateMatchCardsStatsRequest;
+use App\Models\MatchGoalStats;
+use Illuminate\Http\Request;
 
-class MatchCardsStatsController extends Controller
+class MatchGoalStatssController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $MatchGoalStats;
+
+    public function __construct(MatchGoalStats $MatchGoalStats)
+    {
+        $this->MatchGoalStats = $MatchGoalStats;
+    }
+
     public function index()
     {
-        //
+        try {
+            $MatchGoalStats = $this->MatchGoalStats->all();
+            return $MatchGoalStats;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to retrieve match cards stats'], 500);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function show($id)
     {
-        //
+        try {
+            $MatchGoalStats = $this->MatchGoalStats->find($id);
+
+            if ($MatchGoalStats === null) {
+                return response()->json(['error' => 'Match cards stat not found'], 404);
+            }
+
+            return $MatchGoalStats;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to retrieve match cards stat'], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreMatchCardsStatsRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreMatchCardsStatsRequest $request)
+    public function store(Request $request)
     {
-        //
+        try {
+            $data = $request->all();
+
+            $request->validate($this->MatchGoalStats->rules(), $this->MatchGoalStats->feedback());
+
+            $MatchGoalStats = new MatchGoalStats([
+                'minute' => isset($data['minute']) ? $data['minute'] : null,
+                'card_yellow' => isset($data['card_yellow']) ? $data['card_yellow'] : false,
+                'card_red' => isset($data['card_red']) ? $data['card_red'] : false,
+                'soccer_match_id' => isset($data['soccer_match_id']) ? $data['soccer_match_id'] : null,
+                'player_commit_id' => isset($data['player_commit_id']) ? $data['player_commit_id'] : null,
+                'player_suffer_id' => isset($data['player_suffer_id']) ? $data['player_suffer_id'] : null,
+            ]);
+
+            $MatchGoalStats->save();
+
+            return response()->json([
+                'msg' => 'Match cards stat created successfully',
+                'match_cards_stat' => $MatchGoalStats
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to create match cards stat'], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\MatchCardsStats  $matchCardsStats
-     * @return \Illuminate\Http\Response
-     */
-    public function show(MatchCardsStats $matchCardsStats)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $MatchGoalStats = $this->MatchGoalStats->find($id);
+
+            if ($MatchGoalStats === null) {
+                return response()->json(['error' => 'Match cards stat not found'], 404);
+            }
+
+            if ($request->method() === "PATCH") {
+                $requestData = $request->all();
+
+                $rules = array();
+                foreach ($MatchGoalStats->rules() as $input => $rule) {
+                    if (array_key_exists($input, $requestData)) {
+                        $rules[$input] = $rule;
+                    }
+                }
+
+                $request->validate($rules, $MatchGoalStats->feedback());
+            } else {
+                $request->validate($MatchGoalStats->rules(), $MatchGoalStats->feedback());
+            }
+
+            $MatchGoalStats->update($request->all());
+
+            return response()->json([
+                'msg' => 'Match cards stat updated successfully',
+                'match_cards_stat' => $MatchGoalStats
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to update match cards stat'], 500);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\MatchCardsStats  $matchCardsStats
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(MatchCardsStats $matchCardsStats)
+    public function destroy($id)
     {
-        //
-    }
+        try {
+            $MatchGoalStats = $this->MatchGoalStats->find($id);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateMatchCardsStatsRequest  $request
-     * @param  \App\Models\MatchCardsStats  $matchCardsStats
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateMatchCardsStatsRequest $request, MatchCardsStats $matchCardsStats)
-    {
-        //
-    }
+            if ($MatchGoalStats === null) {
+                return response()->json(['error' => 'Match cards stat not found'], 404);
+            }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\MatchCardsStats  $matchCardsStats
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(MatchCardsStats $matchCardsStats)
-    {
-        //
-    }
+            $MatchGoalStats->delete();
+
+            return response()->json(['msg' => 'Match cards stat deleted successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to delete match cards stat'], 500);
+        }
+    } 
 }
--- app/Models/MatchGoalStats.php
+++ app/Models/MatchGoalStats.php
@@ -20,6 +20,31 @@ class MatchGoalStats extends Model
         'player_assist_id',
     ];
 
+    public function rules()
+    {
+        return [
+            'minute' => 'required|integer',
+            'awn' => 'boolean',
+            'soccer_match_id' => 'required|exists:soccer_matches,id',
+            'player_goal_id' => 'required|exists:match_lineup,id',
+            'player_assist_id' => 'nullable|exists:match_lineup,id',
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'minute.required' => 'O campo "minute" é obrigatório.',
+            'minute.integer' => 'O campo "minute" deve ser um número inteiro.',
+            'awn.boolean' => 'O campo "awn" deve ser booleano.',
+            'soccer_match_id.required' => 'O campo "soccer_match_id" é obrigatório.',
+            'soccer_match_id.exists' => 'O valor fornecido para o campo "soccer_match_id" não existe na tabela "soccer_matches".',
+            'player_goal_id.required' => 'O campo "player_goal_id" é obrigatório.',
+            'player_goal_id.exists' => 'O valor fornecido para o campo "player_goal_id" não existe na tabela "match_lineup".',
+            'player_assist_id.exists' => 'O valor fornecido para o campo "player_assist_id" não existe na tabela "match_lineup".',
+        ];
+    }
+
     public function soccerMatch()
     {
         return $this->belongsTo(SoccerMatch::class, 'soccer_match_id');
