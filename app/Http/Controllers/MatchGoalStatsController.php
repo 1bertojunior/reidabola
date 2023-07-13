2cb23596d39c4930953257fc8c0995ddd1dbffd2
1bertojunior <hjunior854@gmail.com>
2023-07-12 22:33:27 -0300

    Implement MatchGoalStats in the API with route, model, controller, and seed for [feature/endpoint]

--- app/Http/Controllers/MatchGoalStatsController.php
+++ app/Http/Controllers/MatchGoalStatsController.php
@@ -3,84 +3,116 @@
 namespace App\Http\Controllers;
 
 use App\Models\MatchGoalStats;
-use App\Http\Requests\StoreMatchGoalStatsRequest;
-use App\Http\Requests\UpdateMatchGoalStatsRequest;
+use Illuminate\Http\Request;
 
 class MatchGoalStatsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $MatchGoalStats;
+
+    public function __construct(MatchGoalStats $MatchGoalStats)
+    {
+        $this->MatchGoalStats = $MatchGoalStats;
+    }
+
     public function index()
     {
-        //
+        try {
+            $MatchGoalStatss = $this->MatchGoalStats->all();
+            return $MatchGoalStatss;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to retrieve match goal stats'], 500);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function show($id)
     {
-        //
+        try {
+            $MatchGoalStats = $this->MatchGoalStats->find($id);
+
+            if ($MatchGoalStats === null) {
+                return response()->json(['error' => 'Match goal stat not found'], 404);
+            }
+
+            return $MatchGoalStats;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to retrieve match goal stat'], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreMatchGoalStatsRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreMatchGoalStatsRequest $request)
+    public function store(Request $request)
     {
-        //
+        try {
+            $data = $request->all();
+
+            $MatchGoalStats = new MatchGoalStats([
+                'minute' => isset($data['minute']) ? $data['minute'] : null,
+                'awn' => isset($data['awn']) ? $data['awn'] : false,
+                'soccer_match_id' => isset($data['soccer_match_id']) ? $data['soccer_match_id'] : null,
+                'player_goal_id' => isset($data['player_goal_id']) ? $data['player_goal_id'] : null,
+                'player_assist_id' => isset($data['player_assist_id']) ? $data['player_assist_id'] : null,
+            ]);
+
+            $MatchGoalStats->save();
+
+            return response()->json([
+                'msg' => 'Match goal stat created successfully',
+                'match_goal_stat' => $MatchGoalStats
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to create match goal stat'], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\MatchGoalStats  $matchGoalStats
-     * @return \Illuminate\Http\Response
-     */
-    public function show(MatchGoalStats $matchGoalStats)
+    public function update(Request $request, $id)
     {
-        //
+        try {
+            $MatchGoalStats = $this->MatchGoalStats->find($id);
+
+            if ($MatchGoalStats === null) {
+                return response()->json(['error' => 'Match goal stat not found'], 404);
+            }
+
+            if ($request->method() === "PATCH") {
+                $requestData = $request->all();
+
+                $rules = array();
+                foreach ($MatchGoalStats->rules() as $input => $rule) {
+                    if (array_key_exists($input, $requestData)) {
+                        $rules[$input] = $rule;
+                    }
+                }
+
+                $request->validate($rules, $MatchGoalStats->feedback());
+            } else {
+                $request->validate($MatchGoalStats->rules(), $MatchGoalStats->feedback());
+            }
+
+            $MatchGoalStats->update($request->all());
+
+            return response()->json([
+                'msg' => 'Match goal stat updated successfully',
+                'match_goal_stat' => $MatchGoalStats
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to update match goal stat'], 500);
+        }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\MatchGoalStats  $matchGoalStats
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(MatchGoalStats $matchGoalStats)
+    public function destroy($id)
     {
-        //
+        try {
+            $MatchGoalStats = $this->MatchGoalStats->find($id);
+
+            if ($MatchGoalStats === null) {
+                return response()->json(['error' => 'Match goal stat not found'], 404);
+            }
+
+            $MatchGoalStats->delete();
+
+            return response()->json(['msg' => 'Match goal stat deleted successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to delete match goal stat'], 500);
+        }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateMatchGoalStatsRequest  $request
-     * @param  \App\Models\MatchGoalStats  $matchGoalStats
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateMatchGoalStatsRequest $request, MatchGoalStats $matchGoalStats)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\MatchGoalStats  $matchGoalStats
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(MatchGoalStats $matchGoalStats)
-    {
-        //
-    }
 }
--- app/Models/MatchGoalStats.php
+++ app/Models/MatchGoalStats.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class MatchGoalStats extends Model
 {
     use HasFactory;
+
+    protected $table = 'match_goal_stats';
+    public $timestamps = true;
+
+    protected $fillable = [
+        'minute',
+        'awn',
+        'soccer_match_id',
+        'player_goal_id',
+        'player_assist_id',
+    ];
+
+    public function soccerMatch()
+    {
+        return $this->belongsTo(SoccerMatch::class, 'soccer_match_id');
+    }
+
+    public function playerGoal()
+    {
+        return $this->belongsTo(MatchLineup::class, 'player_goal_id');
+    }
+
+    public function playerAssist()
+    {
+        return $this->belongsTo(MatchLineup::class, 'player_assist_id');
+    }
 }
