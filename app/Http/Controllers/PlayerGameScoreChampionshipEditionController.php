6b4ca0239c3f8ee9c36059287f8eace2e1640936
1bertojunior <hjunior854@gmail.com>
2023-07-12 22:53:55 -0300

    Implement PlayerGameScoreChampionShipEdition in the API with route, model, controller, and seed for [feature/endpoint]

--- app/Http/Controllers/PlayerGameScoreChampionshipEditionController.php
+++ app/Http/Controllers/PlayerGameScoreChampionshipEditionController.php
@@ -3,84 +3,116 @@
 namespace App\Http\Controllers;
 
 use App\Models\PlayerGameScoreChampionshipEdition;
-use App\Http\Requests\StorePlayerGameScoreChampionshipEditionRequest;
-use App\Http\Requests\UpdatePlayerGameScoreChampionshipEditionRequest;
+use Illuminate\Http\Request;
 
 class PlayerGameScoreChampionshipEditionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    private $playerGameScoreChampionshipEdition;
+
+    public function __construct(PlayerGameScoreChampionshipEdition $playerGameScoreChampionshipEdition)
+    {
+        $this->playerGameScoreChampionshipEdition = $playerGameScoreChampionshipEdition;
+    }
+
     public function index()
     {
-        //
+        try {
+            $playerGameScoreChampionshipEditions = $this->playerGameScoreChampionshipEdition->all();
+            return $playerGameScoreChampionshipEditions;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to retrieve player game score championship editions'], 500);
+        }
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function show($id)
     {
-        //
+        try {
+            $playerGameScoreChampionshipEdition = $this->playerGameScoreChampionshipEdition->find($id);
+
+            if ($playerGameScoreChampionshipEdition === null) {
+                return response()->json(['error' => 'Player game score championship edition not found'], 404);
+            }
+
+            return $playerGameScoreChampionshipEdition;
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to retrieve player game score championship edition'], 500);
+        }
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StorePlayerGameScoreChampionshipEditionRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StorePlayerGameScoreChampionshipEditionRequest $request)
+    public function store(Request $request)
     {
-        //
+        try {
+            $data = $request->all();
+
+            $request->validate($this->playerGameScoreChampionshipEdition->rules(), $this->playerGameScoreChampionshipEdition->feedback());
+
+            $playerGameScoreChampionshipEdition = new PlayerGameScoreChampionshipEdition([
+                'score' => $data['score'],
+                'player_game_score_id' => $data['player_game_score_id'],
+                'championship_edition_id' => $data['championship_edition_id'],
+            ]);
+
+            $playerGameScoreChampionshipEdition->save();
+
+            return response()->json([
+                'msg' => 'Player game score championship edition created successfully',
+                'player_game_score_championship_edition' => $playerGameScoreChampionshipEdition
+            ], 201);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to create player game score championship edition'], 500);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\PlayerGameScoreChampionshipEdition  $playerGameScoreChampionshipEdition
-     * @return \Illuminate\Http\Response
-     */
-    public function show(PlayerGameScoreChampionshipEdition $playerGameScoreChampionshipEdition)
+    public function update(Request $request, $id)
     {
-        //
-    }
+        try {
+            $playerGameScoreChampionshipEdition = $this->playerGameScoreChampionshipEdition->find($id);
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\PlayerGameScoreChampionshipEdition  $playerGameScoreChampionshipEdition
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(PlayerGameScoreChampionshipEdition $playerGameScoreChampionshipEdition)
-    {
-        //
-    }
+            if ($playerGameScoreChampionshipEdition === null) {
+                return response()->json(['error' => 'Player game score championship edition not found'], 404);
+            }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdatePlayerGameScoreChampionshipEditionRequest  $request
-     * @param  \App\Models\PlayerGameScoreChampionshipEdition  $playerGameScoreChampionshipEdition
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdatePlayerGameScoreChampionshipEditionRequest $request, PlayerGameScoreChampionshipEdition $playerGameScoreChampionshipEdition)
-    {
-        //
-    }
+            $requestData = $request->all();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\PlayerGameScoreChampionshipEdition  $playerGameScoreChampionshipEdition
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(PlayerGameScoreChampionshipEdition $playerGameScoreChampionshipEdition)
+            if ($request->method() === "PATCH") {
+                $rules = array();
+                foreach ($playerGameScoreChampionshipEdition->rules() as $input => $rule) {
+                    if (array_key_exists($input, $requestData)) {
+                        $rules[$input] = $rule;
+                    }
+                }
+
+                $request->validate($rules, $playerGameScoreChampionshipEdition->feedback());
+            } else {
+                $request->validate($playerGameScoreChampionshipEdition->rules(), $playerGameScoreChampionshipEdition->feedback());
+            }
+
+            $playerGameScoreChampionshipEdition->update($request->all());
+
+            return response()->json([
+                'msg' => 'Player game score championship edition updated successfully',
+                'player_game_score_championship_edition' => $playerGameScoreChampionshipEdition
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to update player game score championship edition'], 500);
+        }
+    }
+    
+
+    public function destroy($id)
     {
-        //
+        try {
+            $playerGameScoreChampionshipEdition = $this->playerGameScoreChampionshipEdition->find($id);
+
+            if ($playerGameScoreChampionshipEdition === null) {
+                return response()->json(['error' => 'Player game score championship edition not found'], 404);
+            }
+
+            $playerGameScoreChampionshipEdition->delete();
+
+            return response()->json(['msg' => 'Player game score championship edition deleted successfully'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to delete player game score championship edition'], 500);
+        }
     }
 }
