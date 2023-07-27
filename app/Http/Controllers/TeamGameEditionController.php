929ffcaed99663ae079884c67aecd8b112db9de6
1bertojunior <hjunior854@gmail.com>
2023-07-26 22:05:30 -0300

    Modifying TeamGameEdition: correcting Controller and implementing Repository

--- app/Http/Controllers/TeamGameEditionController.php
+++ app/Http/Controllers/TeamGameEditionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\TeamGameEdition;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use App\Repositories\TeamGameEditionRepository;
 
 class TeamGameEditionController extends Controller
 {
@@ -14,57 +16,103 @@ class TeamGameEditionController extends Controller
         $this->teamGameEdition = $teamGameEdition;
     }
 
-    public function index()
+    public function index(Request $request)
     {
-        $teamGameEditions = $this->teamGameEdition->with(['teamGame', 'championshipEdition'])->get();
-        return $teamGameEditions;
+        try{
+            $teamGameEditionRepository = new TeamGameEditionRepository($this->teamGameEdition);
+
+            $teamGameEditionRepository
+                ->selectAttributesRelated([
+                    'teamGame',
+                    'championshipEdition'
+                ]);
+
+            if ($request->has('filter')) {
+                $teamGameEditionRepository->filter($request->filter);                
+            }
+
+            if($request->has('att')){
+                $teamGameEditionRepository->selectAttributes($request->att);
+            }
+
+            $result  = $teamGameEditionRepository->getResult();
+            return response()->json( $result, 200 );
+        }catch (\Exception $e) {
+            return response()->json(['error' => 'An error occurred while processing the request.'], 500);
+        }
     }
 
     public function show($id)
     {
-        $teamGameEdition = TeamGameEdition::with('teamGame', 'championshipEdition')->find($id);
-
-        if ($teamGameEdition === null) {
-            return response()->json(['error' => 'Nenhum dado encontrado.'], 404);
+        try {
+            $teamGameEdition = $this->teamGameEdition
+                ->with([
+                    'teamGame',
+                    'championshipEdition'
+                ])->findOrFail($id);
+            return response()->json($teamGameEdition, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Not found.'], 404);
         }
-
-        return $teamGameEdition;
+        
     }
 
     public function store(Request $request)
     {
-        $data = $request->all();
+        try {
+            $this->validate($request, $this->teamGameEdition->rules(), $this->teamGameEdition->feedback());
 
-        $teamGameEdition = TeamGameEdition::create($data);
+            $teamGameEdition = $this->teamGameEdition->create($request->all());
 
-        return $teamGameEdition;
+            return response()->json($teamGameEdition, 201);
+        } catch (ValidationException $e) {
+            return response()->json(['error' => $e->errors()], 400);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Error creating.'], 500);
+        }
     }
 
     public function update(Request $request, $id)
     {
-        $teamGameEdition = TeamGameEdition::find($id);
+        try {
+            $teamGameEdition = $this->teamGameEdition->find($id);
 
-        if ($teamGameEdition === null) {
-            return response()->json(['error' => 'Nenhum dado encontrado.'], 404);
+            if ($teamGameEdition === null) {
+                return response()->json(['error' => 'Not found'], 404);
+            }
+
+            if ($request->method() === "PATCH") {
+                $requestData = $request->all();
+
+                $rules = array();
+                foreach ($teamGameEdition->rules() as $input => $rule) {
+                    if (array_key_exists($input, $requestData)) {
+                        $rules[$input] = $rule;
+                    }
+                }
+
+                $request->validate($rules, $teamGameEdition->feedback());
+            } else {
+                $request->validate($teamGameEdition->rules(), $teamGameEdition->feedback());
+            }
+
+            $teamGameEdition->update($request->all());
+
+            return response()->json($teamGameEdition, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Failed to update'], 500);
         }
-
-        $teamGameEdition->update($request->all());
-
-        return $teamGameEdition;
     }
 
     public function destroy($id)
     {
-        $teamGameEdition = TeamGameEdition::find($id);
-
-        if ($teamGameEdition === null) {
-            return response()->json(['error' => 'Nenhum dado encontrado.'], 404);
+        try {
+            $result = $this->teamGameEdition->find($id);
+            $result = ($result === null) ? 0 : $result->delete();
+            return $result ? response()->json(['message' => 'Successfully removed.'], 200) : response()->json(['error' => 'No data found.'], 404);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error occurred while removing.'], 500);
         }
-
-        $teamGameEdition->delete();
-
-        return ['msg' => 'Removido com sucesso.'];
     }
-
 
 }
---
+++ app/Repositories/TeamGameEditionRepository.php
@@ -0,0 +1,9 @@
+<?php
+
+    namespace App\Repositories;
+
+    class TeamGameEditionRepository extends AbstractRepository{
+        
+    }
+
+?>
