c5b97394b529132cfdbe1782379bb784b009b0f6
1bertojunior <hjunior854@gmail.com>
2023-08-06 11:46:44 -0300

    Disable mensagens erros exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,16 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Database\QueryException;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Http\Exceptions\ThrottleRequestsException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
-use Illuminate\Session\TokenMismatchException;
-use Illuminate\Support\Facades\Log;
+
 
 class Handler extends ExceptionHandler
 {
@@ -31,49 +22,6 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
         
         });
-    }
-
-    public function render($request, Throwable $exception)
-    {
-        if ($exception instanceof ValidationException) {
-            return response()->json(['errors' => $exception->errors()], 422);
-        }
-
-        if ($exception instanceof QueryException) {
-            Log::error('Database Error: ' . $exception->getMessage());
-            return response()->json(['error' => 'Database error'], 500);
-        }
-
-        if ($exception instanceof AuthenticationException) {
-            return response()->json(['error' => 'Unauthenticated'], 401);
-        }
-
-        if ($exception instanceof AuthorizationException) {
-            return response()->json(['error' => 'Unauthorized'], 403);
-        }
-
-        if ($exception instanceof ModelNotFoundException) {
-            return response()->json(['error' => 'Resource not found'], 404);
-        }
-
-        if ($exception instanceof ThrottleRequestsException) {
-            return response()->json(['error' => 'Too Many Requests'], 429);
-        }
-
-        if ($exception instanceof MethodNotAllowedHttpException) {
-            return response()->json(['error' => 'Method not allowed'], 405);
-        }
-
-        if ($exception instanceof TokenMismatchException) {
-            return response()->json(['error' => 'Token mismatch'], 419);
-        }
-
-        if ($exception instanceof HttpException) {
-            return response()->json(['error' => 'Route error'], $exception->getStatusCode());
-        }
-
-        Log::error('Internal Server Error: ' . $exception->getMessage());
-        return response()->json(['error' => 'Internal server error'], 500);
     }
 
 }
