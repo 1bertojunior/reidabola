e4b33a76ceaaf050915eaa3fe081ce21fb0a7cf0
1bertojunior <hjunior854@gmail.com>
2023-08-06 10:38:18 -0300

    Enhanced API Error Handling: Improved error responses and messages for common scenarios

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,37 +4,76 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\ThrottleRequestsException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Support\Facades\Log;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array
-     */
     protected $dontReport = [
-        //
+
     ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
     protected $dontFlash = [
         'password',
         'password_confirmation',
     ];
 
-    /**
-     * Register the exception handling callbacks for the application.
-     *
-     * @return void
-     */
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+        
         });
     }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ValidationException) {
+            return response()->json(['errors' => $exception->errors()], 422);
+        }
+
+        if ($exception instanceof QueryException) {
+            Log::error('Database Error: ' . $exception->getMessage());
+            return response()->json(['error' => 'Database error'], 500);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return response()->json(['error' => 'Unauthenticated'], 401);
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->json(['error' => 'Unauthorized'], 403);
+        }
+
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json(['error' => 'Resource not found'], 404);
+        }
+
+        if ($exception instanceof ThrottleRequestsException) {
+            return response()->json(['error' => 'Too Many Requests'], 429);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return response()->json(['error' => 'Method not allowed'], 405);
+        }
+
+        if ($exception instanceof TokenMismatchException) {
+            return response()->json(['error' => 'Token mismatch'], 419);
+        }
+
+        if ($exception instanceof HttpException) {
+            return response()->json(['error' => 'Route error'], $exception->getStatusCode());
+        }
+
+        Log::error('Internal Server Error: ' . $exception->getMessage());
+        return response()->json(['error' => 'Internal server error'], 500);
+    }
+
 }
