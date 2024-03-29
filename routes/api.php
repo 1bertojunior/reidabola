66d1b9d09cfe7589fd31910d95cc24bac21d20f9
1bertojunior <hjunior854@gmail.com>
2024-03-29 14:42:26 -0300

    feat: Add version information to root route response

--- routes/api.php
+++ routes/api.php
@@ -5,15 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return [
-        "success" => true
+        "success" => true,
+        "version" => env('APP_VERSION')
     ];
 });
 
 // VERSION 1
 Route::prefix('v1')->group(function () {
     // AUTH ROUTES PUBLIC
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
-    Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::post('login', 'App\Http\Controllers\Auth\AuthController@login');
+    Route::post('register', 'App\Http\Controllers\Auth\AuthController@register');
 
     // PROTECTED
     Route::middleware('jwt.auth')->group(function(){
