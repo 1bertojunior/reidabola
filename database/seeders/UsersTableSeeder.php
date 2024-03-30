9cbf7247fc9ad732650ef2d5b851aae3c7d5cf1f
1bertojunior <hjunior854@gmail.com>
2024-03-30 19:07:35 -0300

    feat: Create admin user in seeder using credentials from .env

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -4,27 +4,30 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
     public function run()
     {
+
         User::create([
-            'email' => 'admin@reidabola.com',
-            'first_name' => 'Admin',
-            'last_name' => 'Rei da Bola',
-            'nick' => 'admin',
-            'password' => '$2y$10$oYo/A16beLUup.Sy.8OioO4lR1YyhQhaggzFJR43sXaQLDK6mZeIm',
-            'access_level_id' => 1
+            'email' => env('ADMIN_EMAIL'),
+            'first_name' => env('ADMIN_FIRST_NAME'),
+            'last_name' => env('ADMIN_LAST_NAME'),
+            'nick' => env('ADMIN_NICK'),
+            'password' => Hash::make( env('ADMIN_PASSWORD'),),
+            'access_level_id' => env('ADMIN_ACCESS_LEVEL_ID')
 
         ]);
+
 
         User::create([
             'email' => 'hjunior854@gmail.com',
             'first_name' => 'Humberto',
             'last_name' => 'Júnior',
             'nick' => '1berto_junior',
-            'password' => '$2y$10$/aMD5iKs4g8cnbl8ASon3eWcBusSHG41FivrLUuDPmSgKLmfmnGdu',
+            'password' => Hash::make('Key@2024'),
             'access_level_id' => 2
 
         ]);
@@ -34,7 +37,7 @@ class UsersTableSeeder extends Seeder
             'first_name' => 'Vitor',
             'last_name' => 'Santos',
             'nick' => 'viktorshts',
-            'password' => '$2y$10$oYo/A16beLUup.Sy.8OioO4lR1YyhQhaggzFJR43sXaQLDK6mZeIm',
+            'password' => Hash::make('Key@2024'),
             'access_level_id' => 2
 
         ]);
